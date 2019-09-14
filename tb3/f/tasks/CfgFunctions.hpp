d2721e87e2c1c829adad8001f6974ce54bc90fc1
Andrew Somerville <asmsandrew@googlemail.com>
2019-09-14 22:48:25 +0100

    Reorg of config files

---
+++ tb3/f/tasks/CfgFunctions.hpp
@@ -0,0 +1,12 @@
+class tasks {
+    class initTasks {
+        postInit = 1;
+        file = "tb3\f\tasks\initTasks.sqf";
+    };
+    class createTask {
+        file = "tb3\f\tasks\createTask.sqf";
+    };
+    class removeTask {
+        file = "tb3\f\tasks\removeTask.sqf";
+    };
+};
